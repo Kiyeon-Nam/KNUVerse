e11c57e10796d9146b3450d49d37d52a642adcda
kiyeon <nky3919@gmail.com>
2024-09-17 15:20:27 +0900

    feat: binding

--- app/src/main/java/com/example/knuverse/MainActivity.kt
+++ app/src/main/java/com/example/knuverse/MainActivity.kt
@@ -5,12 +5,21 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.knuverse.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        // 바인딩 객체 획득
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        // 액티비티 화면 출력
+        setContentView(binding.root)
+
+        // 액션바의 내용을 툴바에 적용
+        setSupportActionBar(binding.toolbar)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
