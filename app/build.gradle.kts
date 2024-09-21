5c76ec7a13fbc11bb1d29a9ec5f55ec6b9e4fdbf
katalsye <katalsye46@gmail.com>
2024-09-21 17:50:12 +0900

    /feat: 인증 신청창

---
+++ app/build.gradle.kts
@@ -0,0 +1,68 @@
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("com.google.gms.google-services")
+}
+
+android {
+    namespace = ("com.example.knuverse")
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = ("com.example.knuverse")
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = ("androidx.test.runner.AndroidJUnitRunner")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    viewBinding {
+        enable = true
+    }
+
+    dataBinding {
+        enable = true
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+
+    // Firebase BOM을 사용하여 버전 관리
+    implementation(platform("com.google.firebase:firebase-bom:32.3.1"))
+
+    // Firebase Auth 및 Firestore
+    implementation("com.google.firebase:firebase-auth-ktx")
+    implementation("com.google.firebase:firebase-firestore-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx")
+
+    // Google Play Services Auth
+    implementation ("com.google.android.gms:play-services-auth:20.6.0")
+
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
---
+++ app/src/main/java/com/example/knuverse/CertiActivity.kt
@@ -0,0 +1,124 @@
+package com.example.knuverse
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
+import java.text.SimpleDateFormat
+import java.util.Date
+import androidx.databinding.DataBindingUtil
+import com.example.knuverse.databinding.ActivityCertiBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class CertiActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityCertiBinding
+    private lateinit var uri: Uri
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_certi)
+
+        // Firebase Authentication
+        firebaseAuth = FirebaseAuth.getInstance()
+        val user = firebaseAuth.currentUser
+        if (user != null) {
+            binding.txtUserName.text = getString(R.string.welcome_user, user.displayName)
+        } else {
+            binding.txtUserName.text = getString(R.string.welcome_default)
+        }
+
+        // 뒤로가기
+        binding.btnBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        // 로그아웃 버튼
+        binding.btnlogout.setOnClickListener {
+            firebaseAuth.signOut()  // Firebase 로그아웃 처리
+            Toast.makeText(this, "로그아웃되었습니다", Toast.LENGTH_SHORT).show()
+            binding.txtUserName.text = "사용자님"
+        }
+
+        // 이미지뷰를 눌렀을 경우 앨범을 호출
+        binding.btnPicker.setOnClickListener {
+            val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+            registerForActivityResult.launch(intent)
+        }
+
+        // 등록하기 버튼을 눌렀을 경우
+        binding.btnRegister.setOnClickListener {
+            imageUpload(uri)
+        }
+    }
+
+    private val registerForActivityResult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == RESULT_OK) {
+                uri = result.data?.data!!
+                binding.txtFileStatus.text = "파일 선택 완료"
+            }
+        }
+
+    // 파일을 Firebase Storage에 업로드
+    private fun imageUpload(uri: Uri) {
+        // storage 인스턴스 생성
+        val storage = Firebase.storage
+        // storage 참조
+        val storageRef = storage.getReference("image")
+        // storage에 저장할 파일명 선언
+        val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        val mountainsRef = storageRef.child("${fileName}.png")
+
+        val uploadTask = mountainsRef.putFile(uri)
+        uploadTask.addOnSuccessListener { taskSnapshot ->
+            // 파일 업로드 성공
+            Toast.makeText(this, "인증 요청을 보냈습니다", Toast.LENGTH_SHORT).show()
+        }.addOnFailureListener {
+            // 파일 업로드 실패
+            Toast.makeText(this, "인증 요청을 보냈습니다", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    // Firebase Storage에서 이미지를 다운로드하여 Glide로 ImageView에 표시
+    private fun imageDownload() {
+        // storage 인스턴스 생성
+        val storage = Firebase.storage
+        // storage 참조
+        val storageRef = storage.getReference("image")
+        // storage에서 가져올 파일명 선언
+        val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        val mountainsRef = storageRef.child("${fileName}.png")
+
+        mountainsRef.downloadUrl.addOnSuccessListener { uri ->
+            // 파일 다운로드 성공
+        }.addOnFailureListener {
+            // 파일 다운로드 실패
+            Toast.makeText(this, "이미지 다운로드 실패", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+    private fun imageDelete() { // 이미지 삭제
+        // storage 인스턴스 생성
+        val storage = Firebase.storage
+        // storage 참조
+        val storageRef = storage.getReference("image")
+        // storage에서 삭제 할 파일명
+        val fileName = SimpleDateFormat("yyyyMMddHHmmss").format(Date())
+        val mountainsRef = storageRef.child("${fileName}.png")
+
+        val deleteTask = mountainsRef.delete()
+        deleteTask.addOnCompleteListener {
+            // 파일 삭제 성공
+        }.addOnFailureListener {
+            // 파일 삭제 실패
+        }
+    }
+}
